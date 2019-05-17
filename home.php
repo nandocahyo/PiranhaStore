9512ceb838ec3fd4e3932b87e06c8515abb683ba
nandocahyo <nandocahyo77@gmail.com>
2019-05-17 13:19:50 +0700

    porto3

---
+++ home.php
@@ -0,0 +1,34 @@
+<?php include "head.php"; ?>
+<script type="text/javascript">
+	document.title="Dashboard";
+	document.getElementById('dash').classList.add('active');
+</script>
+<div class="content">
+	<div class="padding">
+		
+		<div class="box">
+			<div class="padding">
+				<i class="fa fa-clock-o"></i>
+				Waktu
+				<span class="status blued"> <?= date("d-m-Y") ?></span>
+			</div>
+		</div>
+		<div class="box">
+			<div class="padding">
+				<i class="fa fa-bars"></i>
+				Data Item
+				<span class="status blued"><?= $root->show_jumlah_barang() ?></span>
+			</div>
+		</div>
+		<div class="box">
+			<div class="padding">
+				<i class="fa fa-book"></i>
+				Laporan
+				<span class="status blued"><?= $root->show_jumlah_trans2() ?></span>
+			</div>
+		</div>
+
+		
+	</div>
+</div>
+<?php include"foot.php" ?>
